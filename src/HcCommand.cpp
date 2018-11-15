cd260ab8d33b9eedc5a578bdb18a97ddb27ebc06
zhaoxiaowei <xiaowei_zhao1013@163.com>
2018-11-15 13:13:26 +0800

    init version

---
+++ src/HcCommand.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file offb_node.cpp
+ * @brief offboard example node, written with mavros version 0.14.2, px4 flight
+ * stack and tested in Gazebo SITL
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Vector3.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "uav_stop_node");
+    ros::NodeHandle nh;
+
+    ros::Publisher stop_uav_pub = nh.advertise<geometry_msgs::Vector3>
+            ("mul/stop", 1);
+
+
+
+    ros::Rate loop_rate(10);
+    ros::spinOnce();
+
+    geometry_msgs::Vector3 stop_command;
+
+    stop_command.x = 0;
+    stop_command.y = 0;
+    stop_command.z = 0;
+
+    int count = 0;
+
+    while(ros::ok()){
+
+        if(count == 0)
+        {
+            cin>>stop_command.x;
+        }
+        if(count <= 5)
+            stop_uav_pub.publish(stop_command);
+
+        if(count > 5)
+        {
+            stop_command.x = 0;
+
+            stop_uav_pub.publish(stop_command);
+        }
+        count ++;
+        if (count == 20) count = 0;
+        ros::spinOnce();
+        loop_rate.sleep();
+
+    }
+
+    return 0;
+}
